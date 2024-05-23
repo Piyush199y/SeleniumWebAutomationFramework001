be8ed814478783ee8a45fada6f543c3929962bde
Piyush199y <piyusha.bansod@gmail.com>
2024-05-23 22:53:32 +0530

    testSeven

--- src/main/java/org/piyush/driver/DriverManager.java
+++ src/main/java/org/piyush/driver/DriverManager.java
@@ -1,5 +1,26 @@
 package org.piyush.driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class DriverManager {
+
+    static WebDriver driver;
+
+    @BeforeMethod
+    static void inIt(){
+        if(driver == null){
+            driver = new EdgeDriver();
+        }
+    }
+
+    @AfterMethod
+    static void down(){
+        if(driver!=null){
+            driver.quit();
+        }
+    }
 
 }
