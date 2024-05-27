bb77309f1db9b34df0fee86bd4f5ca8630032a1c
Piyush199y <piyusha.bansod@gmail.com>
2024-05-27 21:57:54 +0530

    test thirteen

--- src/test/java/org/piyush/tests/TestVWOLoginPOMBaseTest.java
+++ src/test/java/org/piyush/tests/TestVWOLoginPOMBaseTest.java
@@ -1,25 +1,42 @@
 package org.piyush.tests;
+import net.bytebuddy.build.Plugin;
+import org.assertj.core.api.Assertions;
 import org.piyush.basetest.CommonToAllTest;
+import org.piyush.pages.pageObjectModel.dashboardPage_POM;
 import org.piyush.pages.pageObjectModel.loginPage_POM;
+import org.piyush.utils.PropertyReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.PriorityQueue;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class TestVWOLoginPOMBaseTest extends CommonToAllTest {
-    @Test (groups = "Sanity")
+    @Test (groups = "Sanity", priority = 1)
     public void testLoginNegative(){
         loginPage_POM loginPagePom = new loginPage_POM();
         loginPagePom.openVWOUrl();
         String errorMsgtext = loginPagePom.loginVWOInvalidCreds();
         Assert.assertEquals(errorMsgtext, "Your email, password, IP address or location did not match");
+        Assertions.assertThat(errorMsgtext).isNotNull().isNotBlank().contains(PropertyReader.readyKey("error_message"));
 
     }
 
-    @Test (groups = "Sanity")
+    @Test(priority = 2)
     public void testLoginPositive(){
         loginPage_POM loginPagePom = new loginPage_POM();
         loginPagePom.openVWOUrl();
         loginPagePom.loginToVWOWithValidCreds();
-        Assert.assertEquals(errorMsgtext, "Your email, password, IP address or location did not match");
+
+        dashboardPage_POM dashboardPagePom = loginPagePom.afterLoginVWOValidCreds();
+        dashboardPagePom.loggedInUserName();
+        String expected_username = dashboardPagePom.loggedInUserName();
+
+        Assert.assertEquals(expected_username, PropertyReader.readyKey("expected_username"));
+
+        Assertions.assertThat(expected_username).isNotNull().isNotBlank().contains(PropertyReader.readyKey("expected_username"))
+        ;
 
     }
 }
