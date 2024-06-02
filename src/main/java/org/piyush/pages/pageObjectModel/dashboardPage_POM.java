78d8601c7170ff01a920ced142fb9444797ffdb7
Piyush199y <piyusha.bansod@gmail.com>
2024-06-02 22:52:06 +0530

    test TwentySix

--- src/main/java/org/piyush/pages/pageObjectModel/dashboardPage_POM.java
+++ src/main/java/org/piyush/pages/pageObjectModel/dashboardPage_POM.java
@@ -1,17 +1,13 @@
 package org.piyush.pages.pageObjectModel;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.piyush.base.CommonToAllPage;
 
 public class dashboardPage_POM extends CommonToAllPage {
 
-    WebDriver driver;
-
     public dashboardPage_POM(){
-        driver = new EdgeDriver();
-
     }
 
     By userNameOnDashboard = By.cssSelector("[data-qa='lufexuloga']");
