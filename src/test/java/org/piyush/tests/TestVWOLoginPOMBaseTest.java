e09016ec55e3e8f207951d6dd8bf9bfd8da3f110
Piyush199y <piyusha.bansod@gmail.com>
2024-06-19 20:32:21 +0530

    test log4j

--- src/test/java/org/piyush/tests/TestVWOLoginPOMBaseTest.java
+++ src/test/java/org/piyush/tests/TestVWOLoginPOMBaseTest.java
@@ -1,5 +1,7 @@
 package org.piyush.tests;
 import net.bytebuddy.build.Plugin;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.assertj.core.api.Assertions;
 import org.piyush.basetest.CommonToAllTest;
 import org.piyush.pages.pageObjectModel.dashboardPage_POM;
@@ -7,12 +9,13 @@ import org.piyush.pages.pageObjectModel.loginPage_POM;
 import org.piyush.utils.PropertyReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.PriorityQueue;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class TestVWOLoginPOMBaseTest extends CommonToAllTest {
+
+    private static final Logger logger = LogManager.getLogger(TestVWOLoginPOMBaseTest.class);
+
     @Test (groups = "Sanity", priority = 1)
     public void testLoginNegative(){
         loginPage_POM loginPagePom = new loginPage_POM();
@@ -20,6 +23,7 @@ public class TestVWOLoginPOMBaseTest extends CommonToAllTest {
         String errorMsgtext = loginPagePom.loginVWOInvalidCreds();
         Assert.assertEquals(errorMsgtext, "Your email, password, IP address or location did not match");
         Assertions.assertThat(errorMsgtext).isNotNull().isNotBlank().contains(PropertyReader.readyKey("error_message"));
+        logger.info("Failed");
 
     }
 
@@ -37,6 +41,8 @@ public class TestVWOLoginPOMBaseTest extends CommonToAllTest {
 
         Assertions.assertThat(expected_username).isNotNull().isNotBlank().contains(PropertyReader.readyKey("expected_username"))
         ;
+
+        logger.info("Passed");
 
     }
 }
